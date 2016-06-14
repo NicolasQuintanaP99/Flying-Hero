b1faadc39b8de92170c13a29fa883a33777fb023
NicolasQuintanaP99 <nicolascapo42@gmail.com>
2016-06-14 19:23:11 -0300

    heroe inmovil

--- flying_hero/game.rb
+++ flying_hero/game.rb
@@ -1,15 +1,18 @@
 require 'gosu'
 require_relative 'background'
+require_relative 'hero'
 
 class Game < Gosu::Window
 	def initialize  
 		super(900, 550, fullscreen: false)
 		self.caption = 'Flying hero!'
 		@background = Background.new
+		@hero = Hero.new
 	end
 	
 	def draw
 		@background.draw
+		@hero.draw
 	end
 
 	def button_down(id)
