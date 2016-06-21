3d61e60878cebdf304c1d0e3f36df3923e5bac12
NicolasQuintanaP99 <nicolascapo42@gmail.com>
2016-06-21 19:57:57 -0300

    candy con movimiento

--- flying_hero/game.rb
+++ flying_hero/game.rb
@@ -1,34 +1,40 @@
 require 'gosu'
 require_relative 'background'
 require_relative 'hero'
-
+require_relative 'candy'
 class Game < Gosu::Window
-	def initialize  
-		super(900, 550, fullscreen: false)
-		self.caption = 'Flying hero!'
-		@background = Background.new
-		@hero = Hero.new
+  def initialize  
+	super(900, 550, fullscreen: false)
+	self.caption = 'Flying hero!'
+	@background = Background.new
+	@hero = Hero.new
+	@candy = Candy.new(self)
+  end
+
+  def draw
+	@background.draw
+	@hero.draw
+	@candy.draw
+  end
+
+  def button_down(id)
+	if id == Gosu::KbEscape
+	   close
 	end
+  end
+
+  def update
+    	@background.scroll!
+    	if button_down?(Gosu::KbUp)
+	   @hero.move_up!
+        elsif button_down?(Gosu::KbDown) 
+	   @hero.move_down!(height) 
+        end  
+        @candy.scroll!
+
 	
-	def draw
-		@background.draw
-		@hero.draw
-	end
-
-	def button_down(id)
-		if id == Gosu::KbEscape
-		   close
-		end
-	end
-
-	def update
-	    @background.scroll!
-	    if button_down?(Gosu::KbUp)
-		@hero.move_up!
-	    elsif button_down?(Gosu::KbDown) 
-		@hero.move_down!(height) 
-	    end    	    
-	end
+	  	    
+  end
 
 	
 end
